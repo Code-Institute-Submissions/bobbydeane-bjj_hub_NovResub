cb6b213bd42db8b32e72b007cb26b21d46c88a5b
bobbydeane <robertdeane1992@gmail.com>
2022-10-26 21:04:41 +0000

    Fix add product buttons and update/remove buttons in cart

--- bjj_hub/settings.py
+++ bjj_hub/settings.py
@@ -118,12 +118,12 @@ WSGI_APPLICATION = "bjj_hub.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-# DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#    }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+ }
 
 
 """ If DATABASE_URL is in our OS environ
@@ -131,19 +131,19 @@ then use it to setup the database
 else use the default Django sqlite3 setup.
 """
 
-if "DATABASE_URL" in os.environ:
-    DATABASES = {
-        "default": dj_database_url.parse(
-            os.environ.get("DATABASE_URL"), conn_max_age=600
-        )
-    }
-else:
-    DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.sqlite3",
-            "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
-        }
-    }
+# if "DATABASE_URL" in os.environ:
+#     DATABASES = {
+#         "default": dj_database_url.parse(
+#             os.environ.get("DATABASE_URL"), conn_max_age=600
+#         )
+#     }
+# else:
+#     DATABASES = {
+#         "default": {
+#             "ENGINE": "django.db.backends.sqlite3",
+#             "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+#         }
+#     }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
