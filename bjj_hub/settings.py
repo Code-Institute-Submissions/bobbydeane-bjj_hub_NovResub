67582011612689a026ffb0ec02a3876d12fffbf9
bobbydeane <robertdeane1992@gmail.com>
2022-10-27 21:33:29 +0000

    Add postgres db URL to workspace

--- bjj_hub/settings.py
+++ bjj_hub/settings.py
@@ -13,6 +13,9 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 import os
 import dj_database_url
 
+if os.path.exists("env.py"):
+    import env
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -131,19 +134,19 @@ then use it to setup the database
 else use the default Django sqlite3 setup.
 """
 
-# if "DATABASE_URL" in os.environ:
-#     DATABASES = {
-#         "default": dj_database_url.parse(
-#             os.environ.get("DATABASE_URL"), conn_max_age=600
-#         )
-#     }
-# else:
-#     DATABASES = {
-#         "default": {
-#             "ENGINE": "django.db.backends.sqlite3",
-#             "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
-#         }
-#     }
+if "DATABASE_URL" in os.environ:
+    DATABASES = {
+        "default": dj_database_url.parse(
+            os.environ.get("DATABASE_URL"), conn_max_age=600
+        )
+    }
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
